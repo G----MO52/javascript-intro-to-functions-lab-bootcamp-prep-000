d190085c8e0031c7c3f233c81e424950395a185a
Gary Morford <morfordg@mst.edu>
2018-01-18 04:19:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ sayHiToGrandma('I love you, too.');
 
 function sayHiToGrandma(string){
   if (string === "I love you, Grandma.");
-  return "I love you, too.";
+  return "I love you, too. YES INDEED!";
 }
-sayHiToGrandma("I love you, Grandma.");
+sayHiToGrandma("I love you, Grandma")
+
+
+
